f3166fec926a69eac79cfdfcbb95d60f830dc084
onomaaya <r202202226gz@jindai.jp>
2022-11-04 15:04:29 +0900

    20221104

---
+++ de12/python/wikia.py
@@ -0,0 +1,17 @@
+selecta=int(input("気持ち、1.「悲しい」2.「嬉しい」"))
+
+if selecta==1:
+    print("時間")
+    selecta=int(input("1.「朝」2.「昼」3.「夜」"))
+
+    if selecta==1:
+        print("状況")
+        selecta=int(input("1.「屋外」2.「屋内」"))
+
+        if selecta==1:
+            print()
+
+        elif selecta==2:
+            print()
+
+                                   
--- de12/python/worka.py
+++ de12/python/worka.py
@@ -1,54 +1,64 @@
-
-print("私には15歳になる息子がいる。なにやらそろそろ反抗期が始まりそうだ・・・"
-"〜息子帰宅〜"
-"息子「ただいま〜」")
-
-selecta=int(input("1.「おかえり〜ケーキあるよ」2.「おかえり、テストどうだったの？」"))
-
-if selecta==1:
-    print("息子「今おなかすいてないからあとで食べるわ」")
-    selectab=int(input("1.「わかった、あとでたべよう」2.「早く食べなよ」"))
-
-    if selectab==1:
-        print("息子「」")
-        selectaba=int(input("1.「」2.「」"))
-
-        if selectaba==1:
-            print("息子「」")
-
-        elif selectaba==2:
-            print("息子「」")
-
-    elif selectab==2:
-        print("息子「」")
-        selectabb=int(input("1.「」2.「」"))
-
-        if selectabb==1:
-            print("息子「」")
-
-        elif selectabb==2:
-            print("息子「」")
-
-elif selecta==2:
-    print("息子「まあまあ・・・」")
-    selectbb=int(input("1.「次は頑張りなさいね」2.「ちゃんと勉強してるの？」"))
-
-    if selectbb==1:
-        print("あぁ・・・")
-        selectbba=int(input("1.「」2.「」"))
-
-        if selectbba==1:
-            print("息子「」")
-
-        elif selectbba==2:
-            print("息子「」")
-
-    elif selectbb==2:
-        print("うるせぇなぁ・・・")
-        selectbbb=int(input("1.「」2.「」"))
-
-        if selectbbb==1:
-            print("息子「」")
-
-        elif selectbbb==2:
-            print("息子「」")
+ print("私には15歳になる息子がいる。なにやらそろそろ反抗期が始まりそうだ・・・"
+        "〜息子帰宅〜"
+        "息子「ただいま〜」")
+        
+        selecta=int(input("1.「おかえり〜ケーキあるよ」2.「おかえり、テストどうだったの？」"))
+        
+        if selecta==1:
+            print("息子「今おなかすいてないからあとで食べるわ」")
+            selectab=int(input("1.「わかった後で一緒に食べよう！」2.「早く食べな」"))
+        
+            if selectab==1:
+                print("息子「わかった〜」")
+                print("〜1時間後〜")
+                selectaba=int(input("1.「そろそろ食べよう？」2.「まだ〜？」"))
+        
+                if selectaba==1:
+                    print("息子「うん！いいよ！」")
+                    print("母（息子と一緒にケーキを食べることができた。いい子に育ってくれてよかった・・！）")
+        
+                elif selectaba==2:
+                    print("息子「ちょっと待ってて・・・」")
+                    print("母（結局息子と一緒にケーキを食べることができた。まだ反抗期ではないかな・・？）")
+        
+        
+            elif selectab==2:
+                print("息子「わかったって・・・」")
+                selectabb=int(input("1.「食べるとき言ってね」2.「何その言い方」"))
+        
+                if selectabb==1:
+                    print("息子「うん・・・」")
+                    print("母（結局食べてくれたみたい。反抗気ではないか・・・）")
+        
+                elif selectabb==2:
+                    print("息子「・・・」")
+                    print("母（まだケーキに手をつけていないみたい。反抗期・・？）")
+                    
+        
+        elif selecta==2:
+            print("息子「まあまあ・・・」")
+            selectbb=int(input("1.「次は頑張りなさいね」2.「ちゃんと勉強してるの？」"))
+        
+            if selectbb==1:
+                print("息子「あぁ・・・」")
+                selectbba=int(input("1.「応援してるよ」2.「しっかりしなさいよ？」"))
+        
+                if selectbba==1:
+                    print("息子「うん・・・」")
+                    print("母（ちょっと嫌な顔されたけど、まだ反抗期ではないかな・・？）")
+        
+                elif selectbba==2:
+                    print("息子「わかったってば・・・」")
+                    print("母（部屋に戻ってしまった息子。プチ反抗期？）")
+        
+            elif selectbb==2:
+                print("息子「うるせぇなぁ・・・」")
+                selectbbb=int(input("1.「言いすぎちゃった、ごめんね」2.「何その言い方」"))
+        
+                if selectbbb==1:
+                    print("息子「・・・」")
+                    print("母（部屋に戻ってしまった息子。結構反抗期かも・・・）")
+        
+                elif selectbbb==2:
+                    print("息子「うっせぇ！黙れよ！！」ﾊﾞﾝ")
+                    print("母（部屋に戻ってしまった息子。引きこもって3日も出てこない、反抗期になってしまった。）")
